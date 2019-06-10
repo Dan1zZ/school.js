8fc7ac31e3aca21874707e3971679bb4b802f317
Карпов Денис Владимирович <de.karpov@hq.qiwi.com>
2019-06-10 19:05:06 +0300

    up

--- 06_practice_complex_condition/js/script.js
+++ 06_practice_complex_condition/js/script.js
@@ -4,16 +4,16 @@ const rock = 0;
 const scissors = 1;
 const paper = 2;
 
-let player = prompt('Your choice: 0 - rock, 1 - scissors, 2 - paper');
-let computer = Math.floor(Math.random()*2);
+let player = parseInt(prompt('Your choice: 0 - rock, 1 - scissors, 2 - paper'));
+let computer = Math.round(Math.random(0, 2));
+
+ if(((player == rock)&&(computer == scissors))||((player == scissors)&&(computer == paper))||((player == paper)&&(computer == rock))) {
+	console.log('you wins');
+} else if(((player == rock)&&(computer == paper))||((player == scissors)&&(computer == rock))||((player == paper)&&(computer == scissors))) {
+	console.log('you lose');
+} else if(player == computer) {
+	console.log('draw')
+}
 
 console.log(player);
 console.log(computer);
-
- if(((player=rock)&&(computer=scissors))||((player=scissors)&&(computer=paper))||((player=paper)&&(computer=rock))) {
-	console.log('you wins');
-} else if(((player=rock)&&(computer=paper))||((player=scissors)&&(computer=rock))||((player=paper)&&(computer=scissors))) {
-	console.log('you lose');
-} else if(player=computer) {
-	console.log('draw')
-}
